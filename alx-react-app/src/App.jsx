4894ef7a8ab6354129c57b101e6003d7b0d1ae56
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-07-12 14:33:15 +0100

    added UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
+
 function App() {
   return(
     
@@ -9,6 +11,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
