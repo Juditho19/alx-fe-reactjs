7911e1bfc9e7deb6a2e861da2b24880135fc796e
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-07-18 19:29:54 +0100

    added Counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,18 +1,10 @@
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './components/Header.jsx';
-import MainContent from './components/MainContent.jsx';
-import Footer from './components/Footer.jsx'
-import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
   return(
     
     <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
+      <Counter />
     </>
   );
  
