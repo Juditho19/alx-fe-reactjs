6d558142e29d1b6f56cba0f35f4f941aa831350d
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-07-18 19:15:32 +0100

    added styling to footer

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 
 function UserProfile(props){
     return (
-        <div style={{ border: '3px solid gray', padding: '10px', margin: '10px' , backgroundColor:"hsla(0, 0%, 79%, 1.00)" }}>
-            <h2 style={{ color: '#080000ff' , fontSize:"30px" }}>{props.name}</h2>
-            <p style={{ fontWeight: 'bolder' , color: '#080000ff'  }}>Age: <span style={{ fontWeight: 'bold' , color: '#550000ff'  }}>{props.age}</span></p>
-            <p style={{ fontWeight: 'bolder' , color: '#080000ff'  }}>Bio: <span style={{ fontWeight: 'bold' , color: '#550000ff'  }}>{props.bio}</span></p>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h2 style={{ color: 'blue' }}>{props.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+            <p>Bio: {props.bio}</p>
         </div>
     );
 
