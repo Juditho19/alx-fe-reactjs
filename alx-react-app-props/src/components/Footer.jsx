3dd0effd81e854c821df6535b13c87b3028d3ac2
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-07-20 23:49:46 +0100

    added alx-react-app-props

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p style={{padding: '10px'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+            <p style={{color: '#080000ff' , fontSize: '30px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
