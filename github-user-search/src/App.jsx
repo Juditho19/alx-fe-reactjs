17cedd7590870689b9c2563c3325658be93894ec
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-08-03 22:29:08 +0100

    added github-user-search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(){
+  const token = import.meta.env.VITE_APP_GITHUB_API_KEY;
+  return(<div>
+    <h1>GitHub User Search</h1>
+
+  </div>);
+
+}
+export default App;
