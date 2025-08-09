369b269e6acf230807070a06cece3f076dadd892
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-08-09 13:40:16 +0100

    added styling to UserProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,13 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './index.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-red-500">
-      <h1 className="text-white text-4xl font-bold">Hello World!</h1>
-    </div>
+    <>
+      <UserProfile />
+    </>
 
   );
 }
