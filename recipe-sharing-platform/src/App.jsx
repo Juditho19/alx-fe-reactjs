1ed28b4ec5501740dcec952321037d3a064b136a
Jennifer Onyejekwe <onyejekwejennifer@gmail.com>
2025-08-17 19:35:35 +0100

    initial commit

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Recipe Sharing Platform
+      </h1>
+    </main>
+  );
+}
+
+export default App;
